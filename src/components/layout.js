2e708e420030026079ec512bdbe5f1455eb5470f
Matt <mattpezzente@gmail.com>
2019-10-19 20:22:47 -0400

    Transition working when refreshing pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unused-state */
 /* eslint-disable react/jsx-wrap-multilines */
 // eslint-disable-next-line no-unused-vars
 import React from "react";
@@ -12,93 +13,111 @@ import MainNavMobile from "./h31/sections/MainNavMobile";
 // Config
 import _configSite from "./_configSite";
 
-const Layout = ({ children, path = "/" }) => {
-  const excludedPaths = ["/", "", "/free-consultation"];
+class Layout extends React.Component {
+  constructor() {
+    super();
 
-  return (
-    <>
-      <Global
-        styles={css`
-          body {
-            margin: 0;
-            min-height: 100vh;
-            background: linear-gradient(180deg, #0094d1 0%, #49e9ff 100%);
-          }
-          .tl-edges {
-            overflow-x: visible;
-          }
-          .tl-wrapper {
-            float: none;
-          }
-          .current-page {
-            &:after {
-              width: 100% !important;
+    this.state = {
+      path: "/"
+    };
+  }
+
+  componentWillReceiveProps({ path }) {
+    this.setState({ path });
+  }
+
+  render() {
+    const excludedPaths = ["/", "", "/free-consultation"];
+    const { path } = this.state;
+    const { children } = this.props;
+
+    console.log(path);
+
+    return (
+      <>
+        <Global
+          styles={css`
+            body {
+              margin: 0;
+              min-height: 100vh;
+              background: linear-gradient(180deg, #0094d1 0%, #49e9ff 100%);
             }
-          }
-          .current-page-free-consultation {
-            background: #ffffff;
-            color: #069bd5 !important;
-          }
-        `}
-      />
-
-      <MainNavMobile path={path} />
-      <MainNav path={path} />
-
-      <div
-        css={css`
-          position: relative;
-          margin-top: ${path === "/" ? "calc(80vh - 136px)" : "0px"};
-          padding-top: ${excludedPaths.includes(path) ? "0px" : "175px"};
-          transition: 1.5s ease-in-out;
-
-          &:after {
-            display: block;
-            position: absolute;
-            content: "";
-            top: 0;
-            left: 0;
-            height: 100%;
-            width: 100%;
-
-            opacity: ${path !== "/free-consultation" ? "1" : "0"};
-            transition: 1s;
-
-            background: linear-gradient(
-              rgba(255, 255, 255, 0) 0,
-              rgba(255, 255, 255, 0.85) 6vw,
-              rgba(255, 255, 255, 0.8) 100%
-            );
-          }
-
-          @media (max-width: ${_configSite.md}px) {
-            padding-top: ${excludedPaths.includes(path) ? "0px" : "125px"};
-            &:after {
-              background: linear-gradient(
-                rgba(255, 255, 255, 0) 0px,
-                rgba(255, 255, 255, 0.85) 8vw,
-                rgba(255, 255, 255, 0.8) 100%
-              );
+            .tl-edges {
+              overflow-x: visible;
             }
-          }
+            .tl-wrapper {
+              float: none;
+            }
+            .current-page {
+              &:after {
+                width: 100% !important;
+              }
+            }
+            .current-page-free-consultation {
+              background: #ffffff;
+              color: #069bd5 !important;
+            }
+          `}
+        />
 
-          @media (max-width: ${_configSite.xs}px) {
+        <MainNavMobile path={path} />
+        <MainNav path={path} />
+
+        <div
+          css={css`
+            position: relative;
+            margin-top: ${path === "/" ? "calc(80vh - 136px)" : "0px"};
+            padding-top: ${excludedPaths.includes(path) ? "0px" : "175px"};
+            transition: 1.5s ease-in-out;
+
             &:after {
+              display: block;
+              position: absolute;
+              content: "";
+              top: 0;
+              left: 0;
+              height: 100%;
+              width: 100%;
+
+              opacity: ${path !== "/free-consultation" ? "1" : "0"};
+              transition: 1s;
+
               background: linear-gradient(
                 rgba(255, 255, 255, 0) 0,
-                rgba(255, 255, 255, 0.825) 12vw,
+                rgba(255, 255, 255, 0.85) 6vw,
                 rgba(255, 255, 255, 0.8) 100%
               );
             }
-          }
-        `}
-      >
-        <AnimeSiteBackground path={path} />
-        {children}
-      </div>
-      <Footer path={path} />
-    </>
-  );
-};
+
+            @media (max-width: ${_configSite.md}px) {
+              padding-top: ${excludedPaths.includes(path) ? "0px" : "125px"};
+              &:after {
+                background: linear-gradient(
+                  rgba(255, 255, 255, 0) 0px,
+                  rgba(255, 255, 255, 0.85) 8vw,
+                  rgba(255, 255, 255, 0.8) 100%
+                );
+              }
+            }
+
+            @media (max-width: ${_configSite.xs}px) {
+              &:after {
+                background: linear-gradient(
+                  rgba(255, 255, 255, 0) 0,
+                  rgba(255, 255, 255, 0.825) 12vw,
+                  rgba(255, 255, 255, 0.8) 100%
+                );
+              }
+            }
+          `}
+        >
+          <AnimeSiteBackground path={path} />
+          {children}
+        </div>
+        <Footer path={path} />
+      </>
+    );
+  }
+}
 
 export default Layout;
