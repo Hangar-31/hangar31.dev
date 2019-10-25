246e056c997449dec7784abbd0777126cba62ed5
Matt <mattpezzente@gmail.com>
2019-10-25 12:00:42 -0400

    Updated image loading

--- src/components/images/background/ImgLayer3.js
+++ src/components/images/background/ImgLayer3.js
@@ -17,6 +17,7 @@ export default () => (
     `}
     render={data => (
       <Img
+        loading="eager"
         alt="cloud layer for background design"
         fluid={data.placeholderImage.childImageSharp.fluid}
       />
