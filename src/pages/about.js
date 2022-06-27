a1492c5774bd95c648ee7669e855f43e8852aeb6
Matt Pezzente <matt@hangar31.dev>
2022-06-27 16:06:41 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,9 +10,6 @@ import RequestConsultation from "../components/h31/sections/RequestConsultation"
 
 // Images
 import ImgTeamMatthew from "../components/images/team/ImgTeamMatthew";
-import ImgTeamAndrew from "../components/images/team/ImgTeamAndrew";
-import ImgTeamRachel from "../components/images/team/ImgTeamRachel";
-import ImgTeamJennifer from "../components/images/team/ImgTeamJennifer";
 
 // Data
 const bios = [
@@ -23,34 +20,7 @@ const bios = [
     intro:
       '"TIME IS VALUABLE, SO LET’S SPEND IT ON THE THINGS THAT MATTER MOST, LIKE THE PEOPLE AROUND US."',
     bio: [
-      "Matthew is Hangar 31’s Co-Founder, Full-Stack Web Developer, and Project Manager. He loves helping people integrate technology into their needs and objectives, and often finds that one hour of listening can save weeks of misunderstandings."
-    ]
-  },
-  {
-    name: "Rachel Higley",
-    title: "Tech Lead",
-    image: <ImgTeamRachel />,
-    intro: '"Dream bigger than you think is possible."',
-    bio: [
-      "Rachel is our tech lead who takes everything that is beautiful and makes it a reality you can touch. With over 15 years in the industry she is able to bring to life even your biggest dreams."
-    ]
-  },
-  {
-    name: "Andrew Pezzente",
-    title: "Creative Lead",
-    image: <ImgTeamAndrew />,
-    intro: '"DREAMS BECOME REALITY WHEN PURSUED BEYOND THE HORIZON."',
-    bio: [
-      "Andrew is our Creative Lead, and one of the Co-Founders of Hangar 31. He comes with a background in fine arts, digital design, and an enthusiasm for seeing visions brought to reality."
-    ]
-  },
-  {
-    name: "Jennifer Avila",
-    title: "Graphic Designer",
-    image: <ImgTeamJennifer />,
-    intro: '"All we have to decide is what to do with the time given to us."',
-    bio: [
-      "Jennifer enjoys merging function with form through aesthetically creative web designs. She has always had an appreciation for art and design, and now utilizes it to create functional experiences for users across the web. As a designer with Hangar 31, she aims to make the web a more engaging place for every user through her creativity, and artistic talent."
+      "My name is Matt, I am a Web Developer and Project Manager. I created Hangar 31 so I could use my skills and knowledge to help business succeed no matter what size or scale. One thing I enjoy most, is helping people make better businesses decisions by teaching them how to leverage web technology to achieve their goals.
     ]
   }
 ];
